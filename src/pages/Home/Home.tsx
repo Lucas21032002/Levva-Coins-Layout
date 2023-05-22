5d04fe3feeb1f5cfdcb139a5cda966472dce60c9
Lucas Ferreira <lucas6rodrigues@yahoo.com.br>
2023-05-22 11:04:23 -0300

    feat: Estilização da tabela da Home page
    Finalização do layout do projeto Levva Coins.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import { Header } from "../../components/Header";
 import { SearchForm } from "../../components/SearchForm/Index";
 import { Summary } from "../../components/Summary/Index";
-import { HomeWrapper } from "./style";
+import { HomeWrapper, PriceHighlight, TransactionContainer, TransactionTable } from "./style";
 
 export function Home() {
     return (
@@ -10,6 +10,35 @@ export function Home() {
                 <Header/>
                 <Summary/>
                 <SearchForm/>
+
+                <TransactionContainer>
+                    <TransactionTable>
+                        <thead>
+                            <td>Descrição</td>
+                            <td>Preço</td>
+                            <td>Categoria</td>
+                            <td>Data</td>
+                        </thead>
+                        <tbody>
+                            <tr> {/* Table row, linha da tabela */}
+                                <td width="50%">Desenvolvimento de site</td> {/* coluna da tabela */}
+                                <td>
+                                    <PriceHighlight variant="income">R$12.000,00</PriceHighlight>
+                                </td>
+                                <td>Venda</td>
+                                <td>13/04/2022</td>
+                            </tr>
+                            <tr>
+                                <td width="50%">Almoço</td>
+                                <td>
+                                    <PriceHighlight variant="outcome">R$50,00</PriceHighlight>
+                                </td>
+                                <td>Alimentação</td>
+                                <td>13/04/2022</td>
+                            </tr>
+                        </tbody>
+                    </TransactionTable>
+                </TransactionContainer>
             </HomeWrapper>
         </>
     )
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -4,3 +4,53 @@ export const HomeWrapper = styled.div`
     display: grid;
     grid-template-columns: 1fr 77em 1fr;
 `
+
+export const TransactionContainer = styled.main`
+    grid-column: 2 / 3;
+
+    width: 100%;
+    max-width: 1120px;
+    margin: 1rem auto 0;
+    paddign: 1.5rem
+`;
+
+export const TransactionTable = styled.table`
+    width: 100%;
+    border-collapse: separate;
+    border-spacing: 0 0.5rem;
+    
+    td {
+        
+
+        &:first-child {
+            border-top-left-radius: 6px;
+            border-bottom-left-radius: 6px;
+        }
+
+        &:last-child {
+            border-top-right-radius: 6px;
+            border-bottom-right-radius: 6px;
+        }
+    }
+
+    thead {
+        td {
+            padding: 1rem 2rem;
+        }
+    }
+
+    tbody {
+        td {
+            padding: 1.25rem 2rem;
+            background: ${(props) => props.theme["gray-500"]};
+        }
+    }
+`;
+
+interface PriceHighlightProps {
+    variant: "income" | "outcome";
+}
+
+export const PriceHighlight = styled.span<PriceHighlightProps>`
+    color: ${(props) => props.variant === "income" ? props.theme["gren-500"] : props.theme["red-500"]};
+`;
