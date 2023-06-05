d46c6ebbabeef58065e4012036920bbc990d3708
Lucas Ferreira <lucas6rodrigues@yahoo.com.br>
2023-06-05 10:26:13 -0300

    feat: Implementação da lógica de entradas e saídas
    Faz o cálculo correto em relação as entradas e saídas da aplicação

--- src/components/Summary/Index.tsx
+++ src/components/Summary/Index.tsx
@@ -1,8 +1,34 @@
+import { useStore } from "effector-react";
+
+import TransactionStore from "../../stores/TransactionStore/TransactionStore";
+
 import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from "phosphor-react";
 import { SummaryCard, SummaryContainer } from "./style";
 import { defaultTheme } from "../../styles/defaultTheme";
 
 export function Summary() {
+    const { transactions } = useStore(TransactionStore);
+
+    const summary = transactions.reduce((acc, transaction) => {
+        if(transaction.type === 0 ) {
+            acc.deposits += transaction.amount;
+            acc.total += transaction.amount
+        } else {
+            acc.withdraws += transaction.amount;
+            acc.total -= transaction.amount
+        }
+        return acc 
+    },{
+        deposits: 0,
+        withdraws: 0,
+        total: 0
+    })
+
+    const moneyFormat = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    })
+
     return (
         <SummaryContainer>
             <SummaryCard>
@@ -10,21 +36,21 @@ export function Summary() {
                     <span>Entradas</span>
                     <ArrowCircleUp size={32} color={defaultTheme["yellow-500"]} />
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{moneyFormat.format(summary.deposits)}</strong>
             </SummaryCard>
             <SummaryCard>
                 <header>
                     <span>Saídas</span>
                     <ArrowCircleDown size={32} color={defaultTheme["red-500"]} />
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{moneyFormat.format(summary.withdraws)}</strong>
             </SummaryCard>
             <SummaryCard variant="balance">
                 <header>
                     <span>Total</span>
                     <CurrencyDollar size={32} color={defaultTheme["yellow-500"]}  />
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{moneyFormat.format(summary.total)}</strong>
             </SummaryCard>
         </SummaryContainer>
     )
